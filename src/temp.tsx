8c61156c0136bb3d4e9ca57e3f8381f6e86e60db
Daryna Savytska <dasav@smart-it.com>
2023-03-08 11:07:17 +0200

    Update version

--- src/temp.tsx
+++ src/temp.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 // https://mobile.ant.design/components/picker#datepicker
 // https://mini.ant.design/components/date-picker#rangepicker
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 // import { DateTimeRangePicker } from 'react-datetime-range-super-picker'; // что то калечное, не понятно, что хочет
 // import 'react-datetime-range-super-picker/dist/index.css';
 // https://dishant15.github.io/react-datetime-range-super-picker/
@@ -11,16 +13,20 @@ import React from 'react';
 // const [to_date, setToDate] = useState(new Date());
 
 // const handleFromDateUpdate = ({ date }: any) => {
-//   setFromDate(date.date);
-// };
-// const handleToDateUpdate = ({ date }: any) => {
-//   setToDate(date.date);
-// };
-{/* <div>
-  <DateTimeRangePicker
-    from_date={from_date}
+    //   setFromDate(date.date);
+    // };
+    // const handleToDateUpdate = ({ date }: any) => {
+        //   setToDate(date.date);
+        // };
+        {/* <div>
+        <DateTimeRangePicker
+        from_date={from_date}
     to_date={to_date}
     onFromDateTimeUpdate={handleFromDateUpdate}
     onToDateTimeUpdate={handleToDateUpdate}
-  />
+    />
 </div>; */}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// https://rsuitejs.com/components/date-range-picker/
