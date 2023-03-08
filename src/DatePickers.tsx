6d35d7a98a10330e89777b1e61444f0f6c88eda0
Daryna Savytska <dasav@smart-it.com>
2023-03-08 14:40:41 +0200

    Add rsuite datepicker

--- src/DatePickers.tsx
+++ src/DatePickers.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { DatePicker as antdDatePicker } from 'antd';
+import { DateRangePicker } from 'rsuite';
+import 'rsuite/dist/rsuite.css';
+import './styles.scss';
 
 const { RangePicker } = antdDatePicker;
 
@@ -10,6 +13,17 @@ const DatePickers: React.FC = () => {
       <div>
         <p>Hello, I'm DatePicker for desktop by antd</p>
         <RangePicker showTime />
+      </div>
+      <div>
+        <p>Hello, I'm DatePicker by rsuite</p>
+        <DateRangePicker
+          showMeridian
+          format='yyyy-MM-dd HH:mm:ss'
+          defaultCalendarValue={[
+            new Date('2022-02-01 00:00:00'),
+            new Date('2022-03-01 23:59:59'),
+          ]}
+        />
       </div>
     </div>
   );
